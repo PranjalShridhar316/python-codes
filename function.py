1f4a9426126f91cd35880997d83a7030215b42e7
PranjalShridhar316 <pranjalshridhar9259@gmail.com>
2026-07-08 22:07:32 +0530

    function demonstration

---
+++ function.py
@@ -0,0 +1,15 @@
+#a program with 2 functions
+def add(x,y):
+    """
+    this function takes two numbers and find their sum
+    It displays the sum as a result
+    """
+    print("sum=", (x+y))
+def message():
+    """
+    This function is to display a message
+    """
+print("Welcome to Python")
+
+add(10,25)
+message()        
