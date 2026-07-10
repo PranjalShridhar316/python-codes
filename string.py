543b32b9fbdbdfee357691ec8c2ff84f904eb894
PranjalShridhar316 <pranjalshridhar9259@gmail.com>
2026-07-10 21:44:34 +0530

    program to demonstrate string and indexing

---
+++ string.py
@@ -0,0 +1,5 @@
+s="This program is to demonstrate string data type"
+print(s)
+print(s[0])#it prints the first letter of the string
+print(s[3:7])#it prints form the fourth letter to the seventh letter of the string
+print(s[-1])#it prints the last letter of the string
