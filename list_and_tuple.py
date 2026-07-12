8e61d2bb879a83ac52e59d2114dd1d4051a07063
PranjalShridhar316 <pranjalshridhar9259@gmail.com>
2026-07-12 11:38:30 +0530

    list and tuple demonstrated

---
+++ list_and_tuple.py
@@ -0,0 +1,9 @@
+#list
+list=[1.2,2,3,'thala','sam']
+print("list and be modified and can grow dynamically in memory , list stores different type of elements and array can store only one type of element")
+print(list)
+
+#tuple
+tpl=(1,2,3,'thala','tam')
+print("tuple can not be modified,thus it is not as flexible as list")
+print(tpl)
